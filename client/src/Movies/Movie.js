55a62580bda8c44847dd9fe01d952e6e77e72516
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-02-25 13:40:54 -1000

    finished MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+  const params = useParams()
+  console.log(params)
  
   useEffect(() => {
-    const id = 1;
+    
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
-        .get(`http://localhost:5000/api/movies/${id}`)
+        .get(`http://localhost:5000/api/movies/${params.movieID}`)
         .then(response => {
+          console.log(response)
           setMovie(response.data);
         })
         .catch(error => {
           console.error(error);
         });
 
-  },[]);
+  },[params.movieID]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -27,6 +33,7 @@ const Movie = (props) => {
   // }
 
   if (!movie) {
+    console.log(movie)
     return <div>Loading movie information...</div>;
   }
 
